9bdaf9cafa2ca52940ee944d2603618e7e8ecd64
onursokullu <sokulluonur@hotmail.com>
2021-09-30 09:27:05 +0300

    first-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CovidService } from './Services/covid.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'covidapp';
+
+  columnNames =["Tarih","İstanbul","Ankara","İzmir","Konya","Antalya"];
+  options:any={legend:{position:"Bottom"}};
+
+  constructor(public covidService: CovidService){ }
+  ngOnInit(): void {
+    this.covidService.startConnection();
+    this.covidService.startListener();
+
+  }
+  
+
 }
